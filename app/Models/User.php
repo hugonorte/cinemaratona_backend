dc7729e2b3bbaaa18bc7417cd836611fd87d573e
Hugo Norte <hugonorte@gmail.com>
2025-05-31 22:25:27 -0300

    creating movie and review models and migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,9 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
